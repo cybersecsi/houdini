b1436f5f658b081ff81a5e9b13d9f7b77e711fdf
Angelo Delicato <angelo.delicato@secsi.io>
2022-01-11 16:40:18 +0100

    Fix SEO

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <footer className="text-white body-font bg-gray-900 py-8">
                 <div className="container lg:max-w-5xl px-5 py-8 mx-auto flex items-center md:flex-row flex-col">
                     <div className="flex title-font font-medium items-center text-gray-900 mb-0">
-                        <img src={SecsiLogo} className="w-8 h-8" />
+                        <img src={SecsiLogo} className="w-8 h-8" alt="SecSI Logo"/>
                         <span className="ml-3 text-sm font-bold uppercase tracking-tight text-white">HOUDINI</span>
                     </div>
 
@@ -16,20 +16,18 @@ const Footer = () => {
                     </p>
 
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                        <a className="ml-3 text-gray-500" href="https://twitter.com/cybersecsi" target="_blank">
+                        <a className="ml-3 text-gray-500" href="https://twitter.com/cybersecsi" target="_blank" rel="noopener noreferrer">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                             </svg>
                         </a>
 
-                        <a className="ml-3 text-gray-500" href="https://github.com/cybersecsi" target="_blank">
+                        <a className="ml-3 text-gray-500" href="https://github.com/cybersecsi" target="_blank" rel="noopener noreferrer">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
                             </svg>
                         </a>
-
                     </span>
-
                 </div>
             </footer>
         </>
