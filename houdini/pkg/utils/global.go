7c9435026097075e7303fd17b2dd6b105e093bc2
thelicato <thelicato@users.noreply.github.com>
2023-04-24 16:45:36 +0200

    refactor: remove CLI from main repo

--- houdini/pkg/utils/global.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import (
-	"github.com/cybersecsi/HOUDINI/houdini/pkg/types"
-)
-
-var CurrentPrompt string = "0"
-var Tools []types.Tool
-var DStdout DynamicStdout = DynamicStdout{}
